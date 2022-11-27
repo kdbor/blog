a2c2417812d675d1e3f79e69c33456180832fdf4
Ubuntu <ubuntu@ip-172-31-86-212.ec2.internal>
2022-11-27 19:06:13 +0000

    debug gunicorn error

--- dkbor_blog/settings.py
+++ dkbor_blog/settings.py
@@ -18,7 +18,7 @@ TEMPLATE_DIR = os.path.join(BASE_DIR, "templates")
 SECRET_KEY = os.getenv("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ["*"]
 
