52b6a7a78897d0ae64445d3f422287f4fe527651
Ubuntu <ubuntu@ip-172-31-86-212.ec2.internal>
2022-11-27 19:20:59 +0000

    debug gunicorn error

--- dkbor_blog/settings.py
+++ dkbor_blog/settings.py
@@ -15,7 +15,10 @@ TEMPLATE_DIR = os.path.join(BASE_DIR, "templates")
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING:postgresql_psycopg2 keep the secret key used in production secret!
-SECRET_KEY = os.getenv("SECRET_KEY")
+try:
+    SECRET_KEY = secr['django_secret']
+except:
+    SECRET_KEY = os.getenv("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
